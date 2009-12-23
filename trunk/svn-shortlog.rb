3b42fe306d5f9faaadde4f1196bf66f459913ac7
martin.ankerl <martin.ankerl@54446e08-efa8-11de-91ba-91c70a792394>
2009-12-23 13:47:49 +0000

    works in 1.8 and 1.9

--- trunk/svn-shortlog.rb
+++ trunk/svn-shortlog.rb
@@ -141,14 +141,14 @@ class Date
   end
 
   def to_local_time
-    to_time(new_offset(DateTime.now.offset-offset), :local)
+    to_time_hack(new_offset(DateTime.now.offset-offset), :local)
   end
 
   private
-  def to_time(dest, method)
+  def to_time_hack(dest, method)
     #Convert a fraction of a day to a number of microseconds
     usec = (dest.sec_fraction * 60 * 60 * 24 * (10**6)).to_i
-    Time.send(method, dest.year, dest.month, dest.day, dest.hour, dest.min, dest.sec, usec)
+    Time.send(method, dest.year, dest.month, dest.day, dest.hour, dest.min, dest.sec)
   end
 end
 
